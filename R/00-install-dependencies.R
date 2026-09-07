3ae9e767cbadf18f9a89a90146fcdd45eee92ee3
rafavdz <joserafael.verduzco-torres@glasgow.ac.uk>
2026-09-07 15:49:08 +0100

    Mute code installing packages

--- R/00-install-dependencies.R
+++ R/00-install-dependencies.R
@@ -1,14 +1,19 @@
-# list packages used in project
-# dput(unique(renv::dependencies()$Package))
-packages <- c(
-  'dplyr',
-  'readr',
-  'sf',
-  'yaml',
-  'ggplot2'
-)
-# install/update packages
-install.packages(packages)
 
-# additional packages installed outside CRAN
-renv::install("https://github.com/urbanbigdatacentre/AccessUK")
+# # Do not run, example of packages to be installed
+# # list packages used in project
+# packages <- c(
+#   'targets', 
+#   'usethis', 
+#   'visNetwork',
+#   'dplyr',
+#   'readr',
+#   'sf',
+#   'yaml',
+#   'ggplot2'
+# )
+# # install/update packages
+# install.packages(packages)
+# 
+# # Do not run
+# # Example of additional packages installed outside CRAN
+# renv::install("https://github.com/urbanbigdatacentre/AccessUK")
